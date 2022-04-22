54da8ca9cc10bec6cbdd6d60b81c02a911722342
karima <timmermann@gmx.at>
2022-04-22 07:33:41 +0200

    Add app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './styles/App.css';
+import React, {Component} from "react";
 import Education from "./components/Education.js"
 import Experience from "./components/Experience.js"
 import Personal from "./components/Personal.js"
@@ -6,6 +7,7 @@ import Personal from "./components/Personal.js"
 function App() {
   return (
     <div className="App">
+        <h1>CV builder</h1>  
         <Personal />
         <Education />
         <Experience />
